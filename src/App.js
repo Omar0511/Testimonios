d4f2e2bf33a00791c0b059195e4ff9e0cca0736e
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-10 10:46:21 -0600

    Mostrando el Componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimonio from './component/Testimonio';
 
 function App() {
   return (
     <div className="App">
-
+      <Testimonio />
     </div>
   );
 }
--- src/component/Testimonio.js
+++ src/component/Testimonio.js
@@ -9,6 +9,16 @@ function Testimonio()
                 src={ require('../img/testimonio-shawn.png') }
                 alt='Fotografía de Persona'
             />
+
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
+                <p className='cargo-testimonitos'>Ingeniería de Software en Shopify</p>
+                <p className='texto-testimonio'>
+                    "Siempre he tenido problemas para aprender JavaScript"
+                </p>
+            </div>
         </div>
     );
 }
+
+export default Testimonio;
